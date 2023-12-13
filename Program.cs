28bf200d160b1ed683f260e5d59c75ad813fb86f
arielmoguillansky <arielmogui92@gmail.com>
2023-12-13 18:16:12 -0300

    todo app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// dotnet publish project.csproj --configuration Release --runtime win-x64 - for generating a .exe file
+Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]remove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = "A";
+
+Console.WriteLine(userInput);
+
+Console.ReadKey();
