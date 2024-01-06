89328066436efc495e22e97881f8326110ee83d1
arielmoguillansky <arielmogui92@gmail.com>
2024-01-06 11:58:38 -0300

    Todo app

--- todo-app/Program.cs
+++ todo-app/Program.cs
@@ -1,5 +1,4 @@
-﻿
-string artText = @".
+﻿string artText = @".
  ______  _____   ____    _____                                   
 /\__  _\/\  __`\/\  _`\ /\  __`\                                 
 \/_/\ \/\ \ \/\ \ \ \/\ \ \ \/\ \         __     _____   _____   
@@ -14,15 +13,32 @@ Console.WriteLine(artText);
 
 string userInput;
 
+// Arrays have size fixed
+// int[] numbers = new int[3];
+// int[] numbers = new int[] {1,2,3,4,5,6};
+// int first_from_end = numbers[^1];
+// int size = numbers.Length
+
+// 2-d array
+// char[,] letters = new char[2,3];
+// char[0,0] = 'A';
+// char[,] = new char[,] { {'A', 'B'}, {'C', 'D'} };
+// int height = letters.GetLength(0);
+// int width = letters.GetLength(1);
+// dotnet publish project.csproj --configuration Release --runtime win-x64 - for generating a .exe file
+
+Console.WriteLine("Hello!");
+List<string> todos = new List<string>();
+
 do
 {
-  // dotnet publish project.csproj --configuration Release --runtime win-x64 - for generating a .exe file
-  Console.WriteLine("Hello!");
+  Console.WriteLine();
   Console.WriteLine("What do you want to do?");
   Console.WriteLine("[S]ee all TODOs");
   Console.WriteLine("[A]dd a TODO");
   Console.WriteLine("[R]remove a TODO");
   Console.WriteLine("[E]xit");
+  Console.WriteLine();
 
   userInput = Console.ReadLine().ToLower(); //capture user's input from console
 
@@ -30,27 +46,88 @@ do
   {
     case "s":
       PrintSelectedOption("see all TODOs");
+      SeeAllTodos();
       break;
     case "a":
       PrintSelectedOption("add TODO");
+      AddTodo();
       break;
     case "r":
       PrintSelectedOption("remove TODO");
+      RemoveTodo();
       break;
     case "e":
-      PrintSelectedOption("exit");
+      Console.WriteLine("Bye!");
       break;
     default:
-      Console.WriteLine("No valid option selected");
+      Console.WriteLine("Invalid choice.");
       break;
   }
 }
 while (userInput[0] != 'e');
 
-Console.ReadKey(); //prevent window to close
+// Console.ReadKey(); //prevent window to close
 
 void PrintSelectedOption(string selectedOption)
 {
   Console.WriteLine("Selected option: " + selectedOption);
+}
+
+void SeeAllTodos()
+{
+  foreach (string todo in todos)
+  {
+    Console.WriteLine($"{todos.IndexOf(todo) + 1}. {todo}");
+  }
+}
+
+void AddTodo()
+{
+  string todoDescription;
+  do
+  {
+    Console.WriteLine("Enter the TODO description");
+    todoDescription = Console.ReadLine();
+    if (todoDescription.Length == 0)
+    {
+      Console.WriteLine("The description cannot be empty.");
+    }
+  }
+  while (todoDescription.Length == 0);
+
+  todos.Add(todoDescription);
+}
+
+void RemoveTodo()
+{
+  string todoIndex;
+  bool isValidInput = false;
+  do
+  {
+    Console.WriteLine("Enter number of TODO item to remove.");
+    todoIndex = Console.ReadLine();
+    bool isNum = int.TryParse(todoIndex, out int num);
+    if (!isNum || num > todos.Count())
+    {
+      Console.WriteLine("Enter a valid number.");
+    }
+    else
+    {
+      isValidInput = true;
+    }
+  }
+  while (!isValidInput);
+
+  string confirm;
+  string todoToRemove = todos.ElementAt(int.Parse(todoIndex) - 1);
+
+  Console.WriteLine($"Are you sure you want to remove {todoToRemove}");
+  confirm = Console.ReadLine().ToLower();
+  if (confirm == "y" || confirm == "yes")
+  {
+    Console.WriteLine("Successfully removed.");
+    todos.RemoveAt(int.Parse(todoIndex) - 1);
+  }
+
 }
 
